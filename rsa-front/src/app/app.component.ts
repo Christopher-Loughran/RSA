902d422cbd04bd11249f8495931c7029d5ff0c43
Christopher-Loughran <chris.lou97@Gmail.com>
2022-01-20 17:01:05 +0100

    started public key gen

--- rsa-front/src/app/app.component.ts
+++ rsa-front/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
 
 
- 
+
 
 @Component({
   selector: 'app-root',
@@ -20,11 +20,11 @@ export class AppComponent implements OnInit{
 
   userlist: Array<any> = [];
   chatHistory = [];
-  
+
   currentMessage: string = ""
   currentInterlocutor: string = ""
   currentChat = [];
-  
+
 
   constructor() {
   }
@@ -37,8 +37,9 @@ export class AppComponent implements OnInit{
 
     //generate public / private keys
 
-    this.socket.emit('setup_user', this.id, this.public_key);
+    this.generatePublicKey(53, 61);
 
+    this.socket.emit('setup_user', this.id, this.public_key);
 
     this.socket.on('message', (sender_id, msg) => {
       console.log("From " + sender_id + " : " + msg);
@@ -59,7 +60,7 @@ export class AppComponent implements OnInit{
   }
 
   displayError(error: Error) {
-    console.error(error.message); 
+    console.error(error.message);
   }
 
 
@@ -95,14 +96,13 @@ export class AppComponent implements OnInit{
       if(dest_id != ""){
 
       //encode message
-      
+
       //send message
       this.socket.emit('message', this.id, dest_id, message);
 
       for(var i in this.chatHistory){
         if(this.chatHistory[i].id == dest_id){
 
-          
 
           this.chatHistory[i].chat.push({'sender': "you", 'message': message});
           this.updateCurrentChat(dest_id);
@@ -165,9 +165,68 @@ export class AppComponent implements OnInit{
     }
     this.currentInterlocutor = "";
     this.currentChat = [];
-
-    
   }
 
-  
+
+  generatePublicKey(p, q){
+    var n = p*q;
+    var m = (p-1)*(q-1);
+
+
+    var e = 0;
+
+    var i = 1; //iterator
+    while(e == 0){
+
+      let candidate = (m*i)+1; //generate e candidate
+      this.checkCoPrime(candidate, m);
+
+      e = 1;
+
+      i++;
+    }
+
+  }
+
+
+  checkCoPrime(a, b){
+
+    console.log("co prime" + a + " " + b)
+    let factors = this.findFactors(a);
+    factors.concat(this.findFactors(b));//create a table containing all the factors of a and b
+
+    factors = factors.sort();
+
+    console.log(factors);
+  }
+
+
+  checkIsPrime(num){
+    for(var i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  }
+
+
+  findFactors(num){
+    var factors = [];
+
+    for(let i = 1; i < num; i++) {//TODO: optimize
+      // check if number is a factor
+      if(num % i == 0) {
+        factors.push(i);
+      }
+    }
+    return factors;
+  }
+
+
+  greatestCommonDivisor(a, b) {
+    if (!b) {
+      return a;
+    }
+    return this.greatestCommonDivisor(b, a % b);
+  }
+
+
 }
